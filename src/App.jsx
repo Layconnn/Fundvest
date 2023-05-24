841ef95624b2a74afaa45445416d17dbcb04e023
khalil <olamilekanjinadu21@gmail.com>
2023-05-24 10:56:34 -0700

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,179 @@
+import React from "react";
+import './App.scss';
+
+function App() {
+
+  return (
+    <> 
+     <div className="xr">
+        <p>9:41</p>
+          <div className="xr__net">
+            <img src="./image/cellular.png" />
+            <img src="./image/Wifi.png" />
+            <img src="./image/Battery.png"  />
+          </div>
+        </div>
+      <div className="nav">
+        <img className="nav__logo" src="./image/fund-icon.png" />
+        <div className="nav__options">
+          <p>SAVE</p>
+          <p>FAQ</p>
+          <p>Contact</p>
+          <div className="nav__options__btn1">Sign up now</div>
+          <div className="nav__options__btn2">Log in</div>
+        </div>
+        <div className="nav__icon">
+          <img src="./image/g5.png"  className="nav__icon__first" />
+          <img src="/image/g8.png" className="nav__icon__second" />
+        </div>
+  </div> 
+
+      <div className="section1">
+        <div className="section1__first">
+          <div className="section1__first__options">
+            <p className="section1__first__options__one">Savings</p>
+            <p className="section1__first__options__one">Dollar Savings</p>
+            <h5>Withdrawals</h5>
+          </div>
+          <p className="section1__money">When money realises that it is good hands, it wants to stay and multiply in those hands</p>
+          <div className="section1__first__last">
+            <div className="section1__first__last__btn">Create an Account</div>
+            <p>Fascinating savings plans that give you the best returns that you can trust</p>
+            <div className="section1__first__last__btn2">Create an Account</div>
+          </div>
+        </div>
+        <img src="./image/real.png"/>
+      </div> 
+
+      
+      <div className="section2">
+        <div className="section2__state">
+          <p className="section2__state__chat">Save with FUNDVEST</p>
+          <p className="section2__state__message">Fundvest is a technology platform that provides you an opportunity to protect and invest your capital and earn interest greater than the inflation rate by investing in high-return businesses in various key sectors of the economy.</p>
+        </div>
+        <div className="section2__images">
+          <img src="./image/logo2.png"  />
+          <img src="./image/logo1.png"  />
+        </div>
+      </div> 
+  
+      <div className="section3">
+        <div className="section3__box1">
+          <div className="section3__box1__container"></div>
+          <p className="section3__box1__type"> 21% returns per annum</p>
+          <p className="section3__box1__message">You get a 21 percent return on your investment per annum for any plan you suscribe to</p>
+        </div>
+        <div className="section3__box2">
+          <div className="section3__box2__container"></div>
+          <p className="section3__box2__type">Safe and secure website</p>
+          <p className="section3__box2__message">Our website is safe and secure and you can trust that your investment is safe with us </p>
+        </div>
+        <div className="section3__box3">
+          <div className="section3__box3__container"></div>
+          <p className="section3__box3__type">Sweet rates</p>
+          <p className="section3__box3__message">We give you the best rates on investments that you can’t get anywhere else.</p>
+        </div>
+        <div className="section3__box4">
+          <div className="section3__box4__container"></div>
+          <p className="section3__box4__type">24x7 Friendly Support</p>
+          <p className="section3__box4__message">We are always available round the clock to attend to every of your needs</p>
+        </div>
+      </div>
+  
+      <div className="section4">
+        <div className="section4__part1">
+          <div className="section4__part1__left">
+            <div className="section4__part1__left__sec">
+              <p className="section4__part1__left__sec__co">Fundvest</p>
+              <p className="section4__part1__left__sec__opt">Naira Savings Account</p>
+            </div>
+            <div className="section4__part1__left__btn">Start Saving</div>
+          </div>
+          <img src="./image/coin.png"/>
+        </div>
+        <div className="section4__part2">
+          <div className="section4__part2__left">
+            <div className="section4__part2__left__sec">
+              <p className="section4__part2__left__sec__co">Fundvest</p>
+              <p className="section4__part2__left__sec__opt">Dollar Savings Account</p>
+            </div>
+            <div className="section4__part2__left__btn">Start Saving</div>
+          </div>
+          <img src="./image/coin.png"/>
+        </div>
+      </div>
+  
+      <div className="section5">
+        <div className="section5__word">
+          <p className="section5__word__one">We serve over 100 users in Nigeria</p>
+          <p className="section5__word__two">Invest with Fundvest to get returns at good rate and security for your funds</p>
+        </div>
+        <img src="./image/big.png" />
+      </div>
+      
+      <div className="section6">
+        <div className="section6__main">
+          <div className="section6__main__prof"><img src="./image/profile.jpg" /></div>
+          <div className="section6__main__cap">
+            <p className="section6__main__cap__one">“ I used to feel too young to start setting money aside, with Fundvest I have realized it is never  too early to get started with your savings.</p>
+            <div className="section6__main__cap__beneath">
+              <span className="section6__main__cap__beneath__one">. Michael Smith </span>
+              <span className="section6__main__cap__beneath__two">on overoming temptation</span>
+            </div>
+          </div>
+        </div>
+        <p className="section6__bottom">Read More Stories</p>
+      </div>
+ 
+      <div className="contact">
+        <p className="contact__text">We're here to make your saving easy</p>
+        <img src="./image/btn.png" />
+      </div>
+       
+       <div className="input">
+          <div className="input__one">
+            <p className="input__one__1st">Sign up for free.</p>
+            <p className="input__one__2nd">Start saving today</p>
+          </div>
+          <div className="input__two">
+            <input type="text" placeholder="Your email..." />
+            <button>SIGN UP FOR FREE</button>
+          </div>
+       </div>
+  
+
+       
+        <div className="footer">
+        <div className="footer__bottom">
+          <div className="footer__bottom__imgage">
+            <img src="./image/white.png" alt="" />
+            <p className="footer__bottom__image__gee">Fundvest is a technology platform that provides you an opportunity to saveand protect your capital and earn interest greater than the inflation rate by investing in high-return businesses in various key sectors of the economy.</p>
+          </div>
+          <div className="footer__bottom__info">
+            <div className="footer__bottom__info__one">
+              <h6>Services</h6>
+              <p>Savings</p>
+              <p>Dollar Savings</p>
+              <p>Withdrawal</p>
+            </div>
+            <div className="footer__bottom__info__two">
+              <h6>Company</h6>
+              <p>About</p>
+              <p>Terms & Conditions</p>
+              <p>Disclaimer</p>
+              <p>Privacy & Poilicy</p>
+            </div>
+            <div className="footer__bottom__info__three">
+              <h6>Help</h6>
+              <p>FAQ</p>
+              <p>Support</p>
+              <p>Contact Us</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
